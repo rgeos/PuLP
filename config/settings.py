80ba687ab027c4bbf30fd2088b4579fdeeec3e6d
George S. Radescu <r.george@epigno.jp>
2017-12-07 16:15:06 +0900

    removed mysql bind

--- config/settings.py
+++ config/settings.py
@@ -5,14 +5,14 @@ LOG_LEVEL = 'DEBUG'
 SECRET_KEY = 'hard_to_guess'
 
 db_postgres = 'postgresql://user:pass@localhost:5432/db'
-db_mysql = 'mysql://user:pass@localhost:3306/db'
+# db_mysql = 'mysql://user:pass@localhost:3306/db'
 db_sqlite = 'sqlite:///../data/US_elections_2016.sqlite'
 
 SQLALCHEMY_DATABASE_URI = db_sqlite
 
 SQLALCHEMY_BINDS = {
 	'sqlite': db_sqlite,
-	'mysql': db_mysql,
+	# 'mysql': db_mysql,
 	'postgres': db_postgres
 }
 SQLALCHEMY_TRACK_MODIFICATIONS = False
